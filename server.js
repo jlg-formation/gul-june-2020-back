254a5ed06d4a69224b2d6f7ae02fff3873507c12
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-25 11:31:53 +0200

    added log to the server

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const serveIndex = require('serve-index');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("req.url", req.url);
+  next();
+});
+
 app.use(express.static('.'));
 app.use(serveIndex('.', { icons: true }));
 

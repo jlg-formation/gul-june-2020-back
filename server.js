7488759f466f0b81d34414df57ea221059756fe8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-26 14:27:02 +0200

    add post method

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const serveIndex = require('serve-index');
 const cors = require('cors');
 
+const articles = [
+  { id: 'a1', name: 'Tournevis', price: 2.34, qty: 110 },
+  { id: 'a2', name: 'Marteau', price: 2.34, qty: 20 },
+  { id: 'a3', name: 'Tournevis cruciforme', price: 2.34, qty: 40 },
+  { id: 'a4', name: 'Pince ', price: 2.34, qty: 45 },
+];
+
+let lastId = 4;
+
 const app = express();
 
 app.use((req, res, next) => {
@@ -10,6 +19,22 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/ws/articles', (req, res) => {
+  res.json(articles);
+});
+
+app.post('/ws/articles', (req, res) => {
+  const article = req.body;
+  console.log('article: ', article);
+  lastId++;
+  article.id = 'a' + lastId;
+  articles.push(article);
+  res.status(201).json(article);
+});
+
 app.use(express.static('./www'));
 app.use(serveIndex('./www', { icons: true }));
 
